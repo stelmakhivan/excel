d694cee665f75ccac6ff2d93892fabe918a0bcb3
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-09-02 20:08:22 +0300

    Routing implemented (#7)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import {ExcelComponent} from '@core/ExcelComponent';
 import {debounce} from '@core/utils';
 import {changeTitle} from '@/redux/actions'
 import {defaultTitle} from '@/constants'
+import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -10,7 +11,7 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options
     })
   }
@@ -24,11 +25,11 @@ export class Header extends ExcelComponent {
     return `
       <input type="text" class="input" value="${title}" />
       <div>
-        <div class="button">
-          <i class="material-icons">delete</i>
+        <div class="button" data-button="remove">
+          <i class="material-icons" data-button="remove">delete</i>
         </div>
-        <div class="button">
-          <i class="material-icons">exit_to_app</i>
+        <div class="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
         </div>
       </div>
     `
@@ -37,5 +38,19 @@ export class Header extends ExcelComponent {
   onInput(event) {
     const $target = $(event.target)
     this.$dispatch(changeTitle($target.text()))
+  }
+
+  onClick(event) {
+    const $target = $(event.target)
+
+    if ($target.data.button === 'remove') {
+      const decision = confirm('Do you confirm the deletion?')
+      if (decision) {
+        localStorage.removeItem('excel:' + ActiveRoute.params)
+        ActiveRoute.navigate('')
+      }
+    } else if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('')
+    }
   }
 }
---
+++ src/core/routes/ActiveRoute.js
@@ -0,0 +1,13 @@
+export class ActiveRoute {
+  static get path() {
+    return window.location.hash.slice(1)
+  }
+
+  static get params() {
+    return ActiveRoute.path.split('/')[1]
+  }
+
+  static navigate(path) {
+    window.location.hash = path
+  }
+}
